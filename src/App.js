f53c1985d071df8a00e5f07feb6de3cbf1bbafed
Ramyalshurafa <ramyshurafa@hotmail.com>
2018-12-10 00:39:15 +0200

    setting up the files

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src="https://www.styled-components.com/static/logo.png" className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://www.styled-components.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn Styled Components
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
